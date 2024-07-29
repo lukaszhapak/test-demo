71d3f8424637c7096aeb04635b3a47ea0cfd04e5
lukasz.hapak <lukaszrdw@gmail.com>
2024-07-29 17:26:08 +0200

    spy spec

---
+++ src/test/groovy/com/example/demo/test/unit/basics/SpySpockSpec.groovy
@@ -0,0 +1,36 @@
+package com.example.demo.test.unit.basics
+
+import com.example.demo.test.unit.basics.customer.Customer
+import com.example.demo.test.unit.basics.customer.CustomerRepository
+import com.example.demo.test.unit.basics.customer.CustomerService
+import com.example.demo.test.unit.basics.number.NumberService
+import spock.lang.Specification
+
+class SpySpockSpec extends Specification {
+
+    NumberService numberService = Spy()
+
+    CustomerRepository customerRepository = Stub()
+    CustomerService customerService = Spy(new CustomerService(customerRepository))
+
+    def "should get number from spy service"() {
+        expect:
+        numberService.returningInt() == 5
+    }
+
+    def "should get customer from spy service with stubbed repository"() {
+        given:
+        customerRepository.findById(1) >> new Customer(1, "John", 23)
+
+        expect:
+        customerService.findById(1).name == "John"
+    }
+
+    def "should get customer from spy service with stubbed service"() {
+        given:
+        customerService.findById(1) >> new Customer(1, "John", 23)
+
+        expect:
+        customerService.findById(1).name == "John"
+    }
+}
